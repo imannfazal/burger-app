b923bc2fcbfbc5a5df2ad62ae2b3cfa9b5da904f
imannfazal <imannfazal@gmail.com>
2024-01-23 20:22:54 +0500

    ingredients burger

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "BurgerIngredients.css"
+
+const BurgerIngredients = (props) => {
+    let ingredient = null;
+    switch (props.type) {
+        case ("bread-bottom"):
+            ingredient = <div className='BreadBottom'></div>
+            break;
+        case ("bread-top"):
+            ingredient = <div className='BreadTop'>
+                <div className='Seeds1'></div>
+                <div className='seeds2'></div>
+            </div>
+            break;
+        case ("meat"):
+            ingredient = <div className='Meat'></div>
+            break;
+        case ("cheese"):
+            ingredient = <div className='Cheese'></div>
+            break;
+        case ("salad"):
+            ingredient = <div className='Salad'></div>
+            break;
+        case ("bacon"):
+            ingredient = <div className='Bacom'></div>
+            break;
+        default:
+            ingredient=null;
+
+    }
+    return ingredient;
+}
+
+export default BurgerIngredients
